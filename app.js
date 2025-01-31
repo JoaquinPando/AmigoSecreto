8653d7b35da2b131600a57a7454cbbb906a79984
JoaquinPando <joaquin_a0603@hotmail.com>
2025-01-31 16:29:56 -0500

    Implemtar función para sortear amigos

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ function agregarAmigo() {
     }
 }
 
+function limpiarLista() {
+    amigos = [];
+    agregarAmigoLista();
+}
+function limpiarResultado() {
+    document.getElementById('resultado').innerHTML = "";
+}
+
 function agregarAmigoLista() {
     let lista = document.getElementById('listaAmigos'); //aqui se va a mostrar la lista de amigos
     lista.innerHTML = "";//limpiar la lista
@@ -33,6 +41,20 @@ function agregarAmigoLista() {
         //agregar el li a la lista
         lista.appendChild(li);
     }
+    
+}
+
+function sortearAmigo() {
+    limpiarResultado()
+    //cromprobar si hay amigos en la lista
+    if (amigos.length == 0) {
+        alert('No hay amigos en la lista');
+        return;
+    }
+    let ganador = Math.floor(Math.random() * amigos.length); //math.floor redondea el numero y match.random genera un numero aleatorio
+    console.log(ganador); //mostrar el numero ganador
+    document.getElementById('resultado').innerHTML = amigos[ganador]; //mostrar el nombre del ganador
+    limpiarLista(); //limpiar la lista de amigos
 }
 
 
