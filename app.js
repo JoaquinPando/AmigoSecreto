324e0e25113c0d86bd0dbab823c9d04882222c5e
JoaquinPando <joaquin_a0603@hotmail.com>
2025-01-31 14:22:20 -0500

    implementar funcion para actulizar lista de amigos

--- app.js
+++ app.js
@@ -16,8 +16,23 @@ function agregarAmigo() {
         amigos.push(nombre);
         //limpiar el input
         document.getElementById('amigo').value = "";
+        //mostrar la lista de amigos
+        agregarAmigoLista()
     }
+}
 
+function agregarAmigoLista() {
+    let lista = document.getElementById('listaAmigos'); //aqui se va a mostrar la lista de amigos
+    lista.innerHTML = "";//limpiar la lista
+    //recorrer el array amigos
+    for (let i = 0; i < amigos.length; i++) {
+        //crear un elemento li
+        let li = document.createElement('li');
+        //agregar el nombre del amigo al li
+        li.innerHTML = amigos[i];
+        //agregar el li a la lista
+        lista.appendChild(li);
+    }
 }
 
 
